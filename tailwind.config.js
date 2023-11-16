429b2207068e638540ab35e395266eed1f3e26c9
vt-hadano <vitorhadano@hotmail.com>
2023-11-16 13:59:29 -0300

    Configuracao da Paleta de Cores do projeto

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,28 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+    colors: {
+      'white':{
+        100: '#E6E6E6',
+        75: '#B6B5B7',
+        50: '#B6B5B7',
+        25: '#57555A',
+        12: '#3E3C41',
+        5: '#312F34'
       },
-    },
+      'black':{
+        100: '#27252B',
+        75: '#575559',
+        50: '#868588',
+        25: '#B6B6B7',
+        12: '#CFCFD0',
+        5: '#DCDCDD'
+      },
+      'cyan':'#1994FF',
+      'pink':'#C91DA3',
+      'purple':'#2A0A61',
+      'yellow':'#FFBB0B'
+    }
   },
   plugins: [],
 }
