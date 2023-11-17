3e6e624142dac70bd4ef2258e20afa0bb9de7172
vt-hadano <vitorhadano@hotmail.com>
2023-11-17 16:34:00 -0300

    Estilizacao Footer.tsx

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -7,41 +7,45 @@ import InstagramIcon from "./icon/InstagramIcon";
 
 export default function Footer() {
     return (
-        <div className="footer">
+        <div className="w-full footer flex justify-between bg-white-100 shadow-[0_-4px_16px_rgb(0,0,0,0.25)] md:px-40 px-16 py-8">
             <span className="logo">
-                <LogoIcon className="md:h-16 md:w-16" />
+                <LogoIcon className="md:h-16 md:w-16 h-8 w-8 fill-black-100 hover:fill-cyan transition-all" />
             </span>
-            <div>
-                <ul className="flex flex-col">
-                    <li className="text-black-50"><Link className="hover:text-cyan" href="/">Home</Link></li>
-                    <li className="text-black-50"><Link className="hover:text-yellow" href="/sobre-mim">Sobre</Link></li>
-                    <li className="text-black-50"><Link className="hover:text-cyan" href="/projetos">Projetos</Link></li>
-                    <li className="text-black-50"><Link className="hover:text-pink" href="/contatos">Contatos</Link></li>
+            <div className="flex gap-4 text-right">
+                <ul className="flex flex-col gap-4">
+                    <li className="text-black-50"><Link className="hover:text-cyan transition-all" href="/">Home</Link></li>
+                    <li className="text-black-50"><Link className="hover:text-yellow transition-all" href="/sobre-mim">Sobre</Link></li>
+                    <li className="text-black-50"><Link className="hover:text-cyan transition-all" href="/projetos">Projetos</Link></li>
+                    <li className="text-black-50"><Link className="hover:text-pink transition-all" href="/contatos">Contatos</Link></li>
                 </ul>
-                <div className="divider"></div>
-                <ul className="flex flex-col">
-                    <li className="flex gap-2">
+                <div className="divider h-full w-0.5 bg-black-50 rounded-full"></div>
+                <ul className="flex flex-col gap-4">
+                    <li>
                         <a href="">
-                            <LinkedinSquareIcon className="h-6 w-6 fill-black-50" />
-                            <p>linkedin.com/in/vitorhadano</p>
+                            <LinkedinSquareIcon parentClassName="gap-2 flex" className="h-6 w-6 fill-black-50 transition-all group-hover:fill-cyan ">
+                            <p className="md:block hidden group-hover:text-cyan">linkedin.com/in/vitorhadano</p>
+                            </LinkedinSquareIcon>
                         </a>
                     </li>
-                    <li className="flex gap-2">
+                    <li>
                         <a href="">
-                            <GitIcon className="h-6 w-6 fill-black-50" />
-                            <p>github.com/vt-hadano</p>
+                            <GitIcon parentClassName="gap-2 flex" className="h-6 w-6 fill-black-50 transition-all group-hover:fill-yellow ">
+                            <p className="md:block hidden group-hover:text-yellow">github.com/vt-hadano</p>
+                            </GitIcon>
                         </a>
                     </li>
-                    <li className="flex gap-2">
+                    <li>
                         <a href="">
-                            <WhatsAppIcon className="h-6 w-6 fill-black-50" />
-                            <p>(11) 9 5925-0795</p>
+                            <WhatsAppIcon parentClassName="gap-2 flex" className="h-6 w-6 fill-black-50 transition-all group-hover:fill-cyan">
+                            <p className="md:block hidden group-hover:text-cyan">(11) 9 5925-0795</p>
+                            </WhatsAppIcon>
                         </a>
                     </li>
-                    <li className="flex gap-2">
+                    <li>
                         <a href="">
-                            <InstagramIcon className="h-6 w-6 fill-black-50" />
-                            <p>@vt_hadano</p>
+                            <InstagramIcon parentClassName="gap-2 flex" className="h-6 w-6 fill-black-50 transition-all group-hover:fill-pink">
+                            <p className="md:block hidden group-hover:text-pink">@vt_hadano</p>
+                            </InstagramIcon>
                         </a>
                     </li>
                 </ul>
