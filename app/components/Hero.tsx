f0d647f9f6e047b542ea7726e74f193dc32420db
vt-hadano <vitorhadano@hotmail.com>
2023-11-18 15:03:28 -0300

    Adicao de responsividade

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -6,35 +6,35 @@ import LinkedinSquareIcon from "./icon/LinkedinSquareIcon";
 export default function Hero() {
     return (
         <div className="pt-20 h-screen">
-            <div className="justify-end items-center h-full w-full pr-40 flex font-altregular text-black-100">
-                <div className="w-[32rem] flex-col flex gap-4">
+            <div className="md:justify-end md:items-center items-end justify-center h-full w-full md:pr-40 pb-10 flex font-altregular text-black-100">
+                <div className="md:w-[32rem] w-[16rem] flex-col flex md:gap-4 gap-2">
                     <div>
-                        <p className="text-3xl">Olá! <span className="font-montserrat">E</span>u sou</p>
-                        <h1 className="font-extrabold text-8xl tracking-[0.2em] leading-none">Vitor</h1>
-                        <h1 className="font-extrabold text-8xl tracking-[0.2em] leading-none">Hadano</h1>
+                        <p className="md:text-3xl text-xl">Olá! <span className="font-montserrat">E</span>u sou</p>
+                        <h1 className="font-extrabold md:text-8xl text-5xl tracking-[0.2em] leading-none">Vitor</h1>
+                        <h1 className="font-extrabold md:text-8xl text-5xl tracking-[0.2em] leading-none">Hadano</h1>
                     </div>
-                    <div className="flex flex-row items-center gap-8">
+                    <div className="flex flex-row items-center md:gap-8 gap-4">
                         <div className="w-full h-0.5 bg-black-100 rounded-full"></div>
-                        <div className="bg-cyan rounded-full p-2 flex justify-center items-center drop-shadow-sm hover:bg-light-cyan transition-all focus:bg-white-100 focus:border-4 focus:border-cyan ">
+                        <div className="bg-cyan rounded-full md:p-2 p-1 flex justify-center items-center drop-shadow-sm hover:bg-light-cyan transition-all ">
                             <a href="https://www.linkedin.com/in/vitorhadano/" target="_blank" rel="noopener noreferrer">
-                                <LinkedinSquareIcon parentClassName="" className="h-4 w-4 fill-white-100 focus:fill-cyan" />
+                                <LinkedinSquareIcon parentClassName="" className="md:h-4 md:w-4 h-3 w-3 fill-white-100" />
                             </a>
                         </div>
-                        <div className="bg-pink rounded-full p-2 flex justify-center items-center drop-shadow-sm hover:bg-light-pink transition-all focus:bg-white-100 focus:border-4 focus:border-pink">
+                        <div className="bg-pink rounded-full md:p-2 p-1 flex justify-center items-center drop-shadow-sm hover:bg-light-pink transition-all">
                             <a href="https://www.linkedin.com/in/vitorhadano/" target="_blank" rel="noopener noreferrer">
-                                <InstagramIcon parentClassName="" className="h-4 w-4 fill-white-100 focus:fill-pink" />
+                                <InstagramIcon parentClassName="" className="md:h-4 md:w-4 h-3 w-3 fill-white-100" />
                             </a>
                         </div>
-                        <div className="bg-purple rounded-full p-2 flex justify-center items-center drop-shadow-sm hover:bg-light-purple transition-all focus:bg-white-100 focus:border-4 focus:border-purple ">
+                        <div className="bg-purple rounded-full md:p-2 p-1 flex justify-center items-center drop-shadow-sm hover:bg-light-purple transition-all ">
                             <a href="https://www.linkedin.com/in/vitorhadano/" target="_blank" rel="noopener noreferrer">
-                                <GitIcon parentClassName="" className="h-4 w-4 fill-white-100 focus:fill-purple" />
+                                <GitIcon parentClassName="" className="md:h-4 md:w-4 h-3 w-3 fill-white-100" />
                             </a>
                         </div>
                         <div className="w-full h-0.5 bg-black-100 rounded-full"></div>
                     </div>
-                    <p className="text-3xl w-full text-justify">Que tal melhorar a experiência do seu site com a minha ajuda?</p>
+                    <p className="md:text-3xl text-sm w-full text-justify">Que tal melhorar a experiência do seu site com a minha ajuda?</p>
                     <a href="#form-contato">
-                        <Button className="drop-shadow-lg py-2 w-full bg-pink rounded-full text-3xl text-white-100 hover:bg-light-pink transition-all focus:bg-white-100 focus:border-4 focus:border-pink focus:text-pink">Vamos lá!</Button>
+                        <Button className="drop-shadow-lg md:py-2 py-1 w-full bg-pink rounded-full md:text-3xl text-md text-white-100 hover:bg-light-pink transition-all focus:bg-white-100 focus:border-4 focus:border-pink focus:text-pink">Vamos lá!</Button>
                     </a>
 
                 </div>
