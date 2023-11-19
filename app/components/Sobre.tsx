3fe6fd05fdac8fde7556787f8284b881118f4d4e
vt-hadano <vitorhadano@hotmail.com>
2023-11-19 16:18:00 -0300

    Adicao de link para pagina sobre-mim

--- app/components/Sobre.tsx
+++ app/components/Sobre.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Sobre() {
     return (
@@ -11,7 +12,7 @@ export default function Sobre() {
                     className="md:w-[46rem] w-[21rem]"
                 />
             </div>
-            <div className="flex flex-col md:w-1/3 w-full md:gap-8 gap-4">
+            <div className="flex flex-col md:w-1/3 w-full md:gap-8 gap-4 items-end">
                 <h3 className="md:text-5xl text-2xl md:text-right text-center font-medium">
                     Sobre Mim
                 </h3>
@@ -20,6 +21,7 @@ export default function Sobre() {
 
                     Meu objetivo é sempre produzir a melhor aplicação utilizando de tecnologias de ponta, mantendo o baixo custo sem a necessidade de comprometer a beleza, funcionalidade ou experiência do seu site.
                 </p>
+                <Link href={"/sobre-mim"}><span className="text-pink hover:text-yellow md:text-xl font-medium text-xs transition-all">saiba mais +</span></Link>
             </div>
         </div>
     );
