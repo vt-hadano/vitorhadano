31cfa66e46afb46967d68a047150984597ef04cf
vt-hadano <vitorhadano@hotmail.com>
2023-11-28 13:29:13 -0300

    estilizacao e animacao de LoadingIcon

--- app/components/GradientBg/GradientBg.tsx
+++ app/components/GradientBg/GradientBg.tsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, {ReactNode} from "react";
 import styles from "./GradientBg.module.css";
 
-const GradientBg: React.FC = () => {
+interface GradientBgProps {
+    children? : ReactNode;
+}
+
+const GradientBg: React.FC<GradientBgProps> = ({children}) => {
     return (
         <div className=" relative w-screen overflow-hidden h-screen bg-white-100">
+            {children}
             <div className={styles.g1 + " z-1 blur-[200px] absolute h-[40rem] w-[40rem] rounded-full bg-cyan"}></div>
             <div className={styles.g2 + " z-1 blur-[200px] absolute h-[40rem] w-[40rem] rounded-full bg-pink"}></div>
             <div className={styles.g3 + " z-1 blur-[200px] absolute h-[40rem] w-[40rem] rounded-full bg-yellow"}></div>
--- app/sobre-mim/page.tsx
+++ app/sobre-mim/page.tsx
@@ -1,8 +1,13 @@
 import GradientBg from "../components/GradientBg/GradientBg";
+import LoadingIcon from "../components/LoadinIcon/LoadingIcon";
 
 
-export default function SobreMim () {
-    return(
-        <GradientBg/>
+export default function SobreMim() {
+    return (
+        <>
+            <GradientBg>
+                <LoadingIcon />
+            </GradientBg>
+        </>
     )
 }
