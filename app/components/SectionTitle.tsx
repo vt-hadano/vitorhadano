7dc1bf3b2fd54742f6c657749fe6a1d8e8f12d43
vt-hadano <vitorhadano@hotmail.com>
2023-11-29 18:10:43 -0300

    Adicao da imagem da seccao de tecnologias

--- app/components/SectionTitle.tsx
+++ app/components/SectionTitle.tsx
@@ -6,9 +6,9 @@ interface SectionTitleProps {
 
 const SectionTitle: React.FC<SectionTitleProps> = ({children}) => {
     return (
-        <div className="flex gap-4">
+        <div className="flex gap-4 items-center justify-center md:px-40 px-20">
             <div className="h-1 w-full bg-black-12 rounded-full"></div>
-            <h1 className="text-semibold text-black-100">
+            <h1 className="md:text-5xl text-2xl md:text-right text-center font-medium text-black-100">
                 {children}
             </h1>
             <div className="h-1 w-full bg-black-12 rounded-full"></div>
--- app/components/Tecnologias.tsx
+++ app/components/Tecnologias.tsx
@@ -1,11 +1,18 @@
 import SectionTitle from "./SectionTitle";
+import Image from "next/image";
 
-export default function Tecnologias () {
+export default function Tecnologias() {
     return (
-        <div className="flex flex-col md:px-40 px-20">
+        <div className="flex flex-col gap-16 py-16">
             <SectionTitle>
                 Tecnologias
             </SectionTitle>
+            <Image
+                src="/img-16.png" alt={"Avatar de Vitor Hadano criando um aplicativo em um laboratório"}
+                width={726}
+                height={775}
+                className="w-full"
+            />
         </div>
     );
 }
