981de5515e7f62d1e703cafd58604e7ce85f1644
vt-hadano <vitorhadano@hotmail.com>
2023-11-30 13:12:55 -0300

    criacao de CardTechnology

--- app/components/SectionTitle.tsx
+++ app/components/SectionTitle.tsx
@@ -6,7 +6,7 @@ interface SectionTitleProps {
 
 const SectionTitle: React.FC<SectionTitleProps> = ({children}) => {
     return (
-        <div className="flex gap-4 items-center justify-center md:px-40 px-20">
+        <div className="flex gap-4 items-center justify-center md:px-40 px-16">
             <div className="h-1 w-full bg-black-12 rounded-full"></div>
             <h1 className="md:text-5xl text-2xl md:text-right text-center font-medium text-black-100">
                 {children}
