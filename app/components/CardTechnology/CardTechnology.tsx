e9caf9dc9f0e45ab093e1a4d78fb47e794410671
vt-hadano <vitorhadano@hotmail.com>
2023-12-01 11:43:49 -0300

    criacao do componente SliderTechnology

---
+++ app/components/CardTechnology/CardTechnology.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./CardTechnology.module.css"
+
+const CardTechnology : React.FC = () => {
+    return (
+        <div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    );
+}
+
+export default CardTechnology;
