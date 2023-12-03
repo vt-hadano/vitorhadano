1e48f4c07ae444182762eae11505174973231660
vt-hadano <vitorhadano@hotmail.com>
2023-12-03 02:01:34 -0300

    construcao do componente CustomInput

--- app/components/CustomInput/CustomInput.tsx
+++ app/components/CustomInput/CustomInput.tsx
@@ -1,30 +1,27 @@
 import React, { InputHTMLAttributes } from "react";
+import styles from "./CustomInput.module.css";
 
 interface CustomInputProps extends InputHTMLAttributes<HTMLInputElement> {
     label: string;
-    hasIcon: boolean;
-  }
+    extraClasses: string;
+}
 
-const CustomInput : React.FC<CustomInputProps> = (inputProps) => {
-    const {type, placeholder, label, hasIcon, name, ...rest} = inputProps;
-
-    const handleIcon = () => {
-
-    }
+const CustomInput: React.FC<CustomInputProps> = (inputProps) => {
+    const { type, placeholder, label, name, extraClasses, ...rest } = inputProps;
 
     const handleInputType = () => {
-        if (placeholder === 'textarea') {
-            return <textarea name={name} className="h-full w-full" placeholder={`${placeholderIcon} ${placeholder}`}></textarea>;
+        if (type === 'textarea') {
+            return <textarea name={name} id={name} className={`p-2 h-full w-full rounded-xl overflow-y-auto ${extraClasses} ${styles.textarea}`} placeholder={`${placeholder}`}></textarea>;
         }
-        return <input {...rest} type={type} name={name} className="h-8 w-full" placeholder={`${placeholderIcon} ${placeholder}`}/>;
+        return <input {...rest} type={type} name={name} id={name} className={`py-2 px-4 w-full rounded-xl ${extraClasses}`} placeholder={`${placeholder}`} />;
     }
-
-    const placeholderIcon = hasIcon? handleIcon : '';
 
     return (
         <div>
-            <label htmlFor={name}>{label}</label>
+            <label htmlFor={name} className="font-medium">{label}</label>
             {handleInputType()}
         </div>
     )
 }
+
+export default CustomInput;
